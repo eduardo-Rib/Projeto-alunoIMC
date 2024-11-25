ab7fd19b7f09143f90ab8b6d3ff9a78f02c9642a
Eduardo Fonseca Ribeiro <eduardo10122005@gmail.com>
2024-11-25 16:53:20 -0300

    adicionando metodos criadores das instruções sql

--- src/main/java/com/example/projetoalunoimc/modelo/Aluno.java
+++ src/main/java/com/example/projetoalunoimc/modelo/Aluno.java
@@ -1,5 +1,10 @@
 package com.example.projetoalunoimc.modelo;
 
+import com.example.projetoalunoimc.dao.Crud;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class Aluno {
     private String cpf;
     private String nome;
@@ -53,5 +58,29 @@ public class Aluno {
 
     public void setAltura(float altura) {
         this.altura = altura;
+    }
+
+    public static void inserirAluno(Aluno aluno){
+        String sql= "INSERT INTO aluno (cpf, nome, dataNascimento, peso, altura) VALUES (?, ?, ?, ?, ?)";
+        Crud.insertDeleteUpdate(sql, aluno, 1);
+    }
+
+    public static void excluirAluno(Aluno aluno){
+        String sql= "DELETE FROM aluno WHERE cpf = ?";
+        Crud.insertDeleteUpdate(sql, aluno, 2);
+    }
+
+    public static void atualizarAluno(Aluno aluno){
+        String sql= "UPDATE aluno SET nome = ?, dataNascimento = ?, peso = ?, altura = ? WHERE cpf = ?";
+        Crud.insertDeleteUpdate(sql, aluno, 3);
+    }
+
+    public static List<Aluno> consultarAluno(Aluno aluno, int n) throws SQLException {
+        String sql= "SELECT * FROM aluno WHERE cpf = ?";
+
+        if (n != 1){
+            sql = "SELECT * FROM aluno";
+        }
+        return Crud.select(sql, aluno, n);
     }
 }
--- src/main/java/com/example/projetoalunoimc/modelo/Colunas.java
+++ src/main/java/com/example/projetoalunoimc/modelo/Colunas.java
@@ -1,5 +1,11 @@
 package com.example.projetoalunoimc.modelo;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Colunas {
 
     private int id;
@@ -26,7 +32,25 @@ public class Colunas {
         this.nome = nome;
     }
 
-    /*public String toString(){
-        return nome;
-    }*/
+    public ObservableList carregarColunas() {
+        List<Colunas> colunas = new ArrayList<>();
+        ObservableList<Colunas> obsColunas;
+
+        colunas.clear();
+        Colunas coluna1 = new Colunas(1, "CPF");
+        Colunas coluna2 = new Colunas(2, "Nome");
+        Colunas coluna3 = new Colunas(3, "DataNascimento");
+        Colunas coluna4 = new Colunas(4, "peso");
+        Colunas coluna5 = new Colunas(5, "altura");
+
+        colunas.add(coluna1);
+        colunas.add(coluna2);
+        colunas.add(coluna3);
+        colunas.add(coluna4);
+        colunas.add(coluna5);
+
+        obsColunas = FXCollections.observableArrayList(colunas);
+
+        return obsColunas;
+    }
 }
