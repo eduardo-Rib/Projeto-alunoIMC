c859e0d7a33adfe8f3382952bf468aa441e4454d
Eduardo Fonseca Ribeiro <eduardo10122005@gmail.com>
2024-11-25 03:10:16 -0300

    Criando classe colunas da tabela de alunos

---
+++ src/main/java/com/example/projetoalunoimc/modelo/Colunas.java
@@ -0,0 +1,32 @@
+package com.example.projetoalunoimc.modelo;
+
+public class Colunas {
+
+    private int id;
+    private String nome;
+
+    public Colunas(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /*public String toString(){
+        return nome;
+    }*/
+}
