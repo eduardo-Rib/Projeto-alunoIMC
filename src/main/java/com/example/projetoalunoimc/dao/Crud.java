c7c2ae39d19e4292d243896a87f7aa3d11e37bac
Eduardo Fonseca Ribeiro <eduardo10122005@gmail.com>
2024-11-25 16:54:01 -0300

    aditando metodos crud

--- src/main/java/com/example/projetoalunoimc/dao/Crud.java
+++ src/main/java/com/example/projetoalunoimc/dao/Crud.java
@@ -13,17 +13,22 @@ import java.util.List;
 
 public class Crud {
 
-    public void insertDeleteUpdate(String sql, Aluno aluno, int n){
+    public static void insertDeleteUpdate(String sql, Aluno aluno, int n){
         Connection con = new Conexao().conectar();
         try (PreparedStatement stmt = con.prepareStatement(sql)) {
             if (n == 2){
                 stmt.setString(1, aluno.getCpf());
-            }else{
+            }else if (n == 1){
                 stmt.setString(1, aluno.getCpf());
+                stmt.setString(2, aluno.getNome());
+                stmt.setString(3, aluno.getDataNascimento());
+                stmt.setFloat(4, aluno.getPeso());
+                stmt.setFloat(5, aluno.getAltura());
+            }else{
                 stmt.setString(1, aluno.getNome());
-                stmt.setString(1, aluno.getDataNascimento());
-                stmt.setFloat(1, aluno.getPeso());
-                stmt.setFloat(1, aluno.getAltura());
+                stmt.setString(2, aluno.getDataNascimento());
+                stmt.setFloat(3, aluno.getPeso());
+                stmt.setFloat(4, aluno.getAltura());
             }
 
             System.out.println(stmt);
@@ -33,14 +38,14 @@ public class Crud {
         }
     }
 
-    public List<Aluno> select(String sql, Aluno aluno) throws SQLException {
+    public static List<Aluno> select(String sql, Aluno aluno, int n) throws SQLException {
         List<Aluno> alunos = new ArrayList<>();
         Connection con = new Conexao().conectar();
 
         try (PreparedStatement stmt = con.prepareStatement(sql)) {
-
-            stmt.setString(1, aluno.getCpf());
-
+            if (n == 1) {
+                stmt.setString(1, aluno.getCpf());
+            }
             System.out.println(stmt);
 
             try (ResultSet result = stmt.executeQuery()) {
