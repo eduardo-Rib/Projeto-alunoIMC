e8966e809cd08b023884b9123044f63e334806ac
Eduardo Fonseca Ribeiro <eduardo10122005@gmail.com>
2024-11-25 03:09:46 -0300

    Adicionando método de consulta no banco de dados

--- src/main/java/com/example/projetoalunoimc/dao/Crud.java
+++ src/main/java/com/example/projetoalunoimc/dao/Crud.java
@@ -5,13 +5,17 @@ import com.example.projetoalunoimc.modelo.Aluno;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Crud {
 
     public void insertDeleteUpdate(String sql, Aluno aluno, int n){
         Connection con = new Conexao().conectar();
-        try (PreparedStatement stmt = con.prepareStatement(sql.toString())) {
+        try (PreparedStatement stmt = con.prepareStatement(sql)) {
             if (n == 2){
                 stmt.setString(1, aluno.getCpf());
             }else{
@@ -29,6 +33,31 @@ public class Crud {
         }
     }
 
-    
+    public List<Aluno> select(String sql, Aluno aluno) throws SQLException {
+        List<Aluno> alunos = new ArrayList<>();
+        Connection con = new Conexao().conectar();
 
+        try (PreparedStatement stmt = con.prepareStatement(sql)) {
+
+            stmt.setString(1, aluno.getCpf());
+
+            System.out.println(stmt);
+
+            try (ResultSet result = stmt.executeQuery()) {
+
+                while (result.next()) {
+                    Aluno aux = new Aluno(result.getString("cpf"), result.getString("nome"), result.getString("dataNascimento"), result.getFloat("peso"), result.getFloat("altura"));
+                    alunos.add(aux);
+                }
+            } catch (SQLException exception) {
+                throw new RuntimeException(exception);
+            } finally {
+                stmt.close();
+            }
+
+            return alunos;
+        }catch (SQLException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
 }
